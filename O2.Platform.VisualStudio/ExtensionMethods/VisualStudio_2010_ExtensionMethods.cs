42e4eb1f47c6d89db705ce43b6a367b7caa80b0b
DinisCruz <dinis.cruz@owasp.org>
2015-07-25 16:34:06 +0000

    Moved O2 VisualStudio extension files into this repo

---
+++ O2.Platform.VisualStudio/ExtensionMethods/VisualStudio_2010_ExtensionMethods.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.Shell;
+using WinForms = System.Windows.Forms;
+
+
+namespace O2.FluentSharp.VisualStudio.ExtensionMethods
+{
+    public static class VisualStudio_2010_ExtensionMethods_Packages
+    {
+    	public static Package package(this VisualStudio_2010 visualStudio)
+    	{
+    		return VisualStudio_2010.Package;
+    	}
+    	public static T getService<T>(this VisualStudio_2010 visualStudio)
+		{
+			return VisualStudio_2010.Package.getService<T>();
+		}
+    }
+ 
+}
---
+++ O2.Platform.VisualStudio/VS_Scripts/O2_Platform_Gui.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using WeifenLuo.WinFormsUI.Docking;
+using O2.Kernel;
+using O2.DotNetWrappers.ExtensionMethods;
+using O2.External.SharpDevelop.ExtensionMethods;
+using O2.FluentSharp.REPL;
+using O2.FluentSharp.VisualStudio;
+
+
+namespace O2.Platform.VisualStudio_2010_Extension
+{    
+    public class O2_Platform_Gui
+    {
+        public static VisualStudio_2010     visualStudio;
+
+        public O2_Platform_Gui()
+        {
+            visualStudio = new VisualStudio_2010();
+        }
+
+        public O2_Platform_Gui buildGui()
+        {            
+            createTopLevelMenu();            
+			return this;
+        }
+
+        public O2_Platform_Gui installO2Scripts()
+        {
+            "Skipping: VisualStudio_O2_Utils.installO2Scripts_IfDoesntExist();".info();
+            //VisualStudio_O2_Utils.installO2Scripts_IfDoesntExist();
+            return this;
+        }
+        public O2_Platform_Gui createTopLevelMenu()
+        {
+            @"VS_O2_Plugins\Create O2 Platform Menu.h2".local().executeH2Script();            
+            return this;
+        }        
+
+        public DockPanel openScriptsViewer()
+        {                        
+            var scriptsFolder = @"VS_O2_PlugIns\Create O2 Platform Menu.h2".local().parentFolder();
+            return scriptsFolder.open_Script_Viewer_GUI();            
+        }                    
+    }
+    
+}
